6777954666a2c9a577ca27e49db071f46b51eb44
Ruben Dario Barrios Montealegre <rubenbarmont@gmail.com>
2025-08-26 10:35:35 -0500

    feature(HU_#01): Add attributes to User class

--- domain/model/src/main/java/co/com/crediya/model/user/User.java
+++ domain/model/src/main/java/co/com/crediya/model/user/User.java
@@ -5,10 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class User {
+    private Long id;
+    private String name;
+    private String lastName;
+    private String birthDate;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private BigDecimal baseSalary;
 }
